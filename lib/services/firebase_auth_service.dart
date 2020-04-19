c52b02e15e83ec3895df8aeaca9d979f5f5eb67e
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-04-19 11:46:04 +0200

    Updated FirebaseAuthService.

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -1,12 +1,14 @@
 import 'package:chatroom/services/i_auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
-class FireBaseAuthService implements IAuthService {
+class FirebaseAuthService implements IAuthService {
   @override
   Future<bool> logIn() async {
     try {
       final result = await FirebaseAuth.instance.signInAnonymously();
       if (result != null) {
+        debugPrint('LoggedIn');
         return true;
       }
     } catch (e) {
@@ -19,6 +21,7 @@ class FireBaseAuthService implements IAuthService {
   Future<void> logOut() async {
     try {
       await FirebaseAuth.instance.signOut();
+      debugPrint('LoggedOut');
     } catch (e) {
       print(e);
     }
