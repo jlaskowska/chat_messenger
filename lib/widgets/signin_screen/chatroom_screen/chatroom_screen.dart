d81535344d8a1d7c13641cf815bc0ff008425f9a
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-04-20 19:18:12 +0200

    Updated ChatroomScreen.

--- lib/widgets/signin_screen/chatroom_screen/chatroom_screen.dart
+++ lib/widgets/signin_screen/chatroom_screen/chatroom_screen.dart
@@ -1,4 +1,8 @@
+import 'package:chatroom/services/device_storage.dart';
+import 'package:chatroom/services/i_auth_service.dart';
+import 'package:chatroom/widgets/signin_screen/signin_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatroomScreen extends StatelessWidget {
   const ChatroomScreen({Key key}) : super(key: key);
@@ -6,10 +10,31 @@ class ChatroomScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Chatroom'),
-        ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Center(
+              child: Text('Chatroom'),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: FlatButton(
+                onPressed: () async {
+                  await Provider.of<IAuthService>(context).logOut();
+                  await Provider.of<DeviceStorage>(context).deleteAll();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => SigninScreen(),
+                    ),
+                  );
+                },
+                child: Text('Log out'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
