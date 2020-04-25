59ba62f2fed9e1e848cda2d2861eb810fe5243a3
Joanna Laskowska <58981559+jlaskowska@users.noreply.github.com>
2020-04-25 17:00:49 +0200

    Feature/chat bubble (#17)
    Resolves #15 and #16
    
    Commits:
    
    * Updated AppColors.
    * Created ChatBubble.
    * Added padding to ChatBubble.
    * Added padding to nickname in ChatBubble.
    * Updated ChatroomScreen with ChatBubbles.

---
+++ lib/widgets/chatroom_screen/chat_bubble.dart
@@ -0,0 +1,81 @@
+import 'package:chatroom/configs/app_colors.dart';
+import 'package:chatroom/models/chat_user.dart';
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  final ChatUser chatuser;
+  final String message;
+  final bool isMe;
+  ChatBubble({
+    Key key,
+    @required this.chatuser,
+    @required this.isMe,
+    @required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print(isMe);
+
+    final avatar = Column(
+      children: <Widget>[
+        Icon(
+          Icons.person,
+          color: isMe ? AppColors.blue : AppColors.lightBlue,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: Text(
+            chatuser.nickname,
+            style: TextStyle(color: AppColors.gray),
+          ),
+        )
+      ],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8),
+      child: Row(
+        mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: <Widget>[
+          // isMe ? avatar : Expanded(child: Container()),
+          if (!isMe)
+            avatar,
+          Column(
+            crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: !isMe
+                      ? const BorderRadius.only(
+                          topRight: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(25.0),
+                          bottomRight: Radius.circular(20.0),
+                        )
+                      : const BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(25.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                  color: isMe ? AppColors.blue : Colors.grey.shade300,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
+                  child: Text(
+                    message,
+                    style: TextStyle(
+                      color: isMe ? AppColors.white : Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // isMe ? Expanded(child: Container()) : avatar,
+          if (isMe)
+            avatar,
+        ],
+      ),
+    );
+  }
+}
