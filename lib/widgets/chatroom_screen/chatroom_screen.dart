94ed370f5563229d777d2ccbb1777045db6e009e
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-04-25 16:54:32 +0200

    Updated ChatroomScreen with ChatBubbles.

--- lib/widgets/chatroom_screen/chatroom_screen.dart
+++ lib/widgets/chatroom_screen/chatroom_screen.dart
@@ -3,6 +3,7 @@ import 'package:chatroom/models/chat_user.dart';
 import 'package:chatroom/services/device_storage.dart';
 import 'package:chatroom/services/i_auth_service.dart';
 import 'package:chatroom/services/i_chat_service.dart';
+import 'package:chatroom/widgets/chatroom_screen/chat_bubble.dart';
 import 'package:chatroom/widgets/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,8 +11,25 @@ import 'package:uuid/uuid.dart';
 
 import '../../localizations.dart';
 
-class ChatroomScreen extends StatelessWidget {
+class ChatroomScreen extends StatefulWidget {
   const ChatroomScreen({Key key}) : super(key: key);
+
+  @override
+  _ChatroomScreenState createState() => _ChatroomScreenState();
+}
+
+class _ChatroomScreenState extends State<ChatroomScreen> {
+  String _currentUserId;
+  String _currentUserNickname;
+  DeviceStorage _deviceStorage;
+
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+    _deviceStorage = Provider.of<DeviceStorage>(context);
+    _currentUserId = await _deviceStorage.getUserID();
+    _currentUserNickname = await _deviceStorage.getUserNickname();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +42,17 @@ class ChatroomScreen extends StatelessWidget {
               stream: Provider.of<IChatService>(context, listen: false).messageStream(),
               builder: (_, AsyncSnapshot<List<ChatMessage>> snapshot) {
                 if (snapshot.hasData) {
-                  return Column(
-                    children: <Widget>[
-                      for (final message in snapshot.data) Text(message.message),
-                    ],
+                  return Expanded(
+                    child: ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          final message = snapshot.data[index];
+                          return ChatBubble(
+                            chatuser: message.chatUser,
+                            isMe: message.chatUser.id == _currentUserId,
+                            message: message.message,
+                          );
+                        }),
                   );
                 }
                 return Container();
@@ -59,8 +84,8 @@ class ChatroomScreen extends StatelessWidget {
                     message: 'bla bla bla',
                     date: DateTime.now(),
                     chatUser: ChatUser(
-                      id: Uuid().v4(),
-                      nickname: 'puszek',
+                      id: _currentUserId,
+                      nickname: _currentUserNickname,
                     ),
                   ),
                 );
