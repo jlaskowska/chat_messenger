d8850b738d49deb3f23053bcea9859671cf3ef60
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-04-25 16:02:15 +0200

    Updated AppColors.

--- lib/configs/app_colors.dart
+++ lib/configs/app_colors.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color blue = Color(0xff75aef5);
+
+  static const Color white = Color(0xfff3f8fb);
+  static const Color lightBlue = Color(0xffa3c7f7);
+  static const Color gray = Color(0xff959698);
 }
